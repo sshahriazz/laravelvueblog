155024efa30fa41401a5639c9d6fba4474049f21
Shahriaz Kabir <34005640+sshahriazz@users.noreply.github.com>
2021-07-03 15:59:22 +0600

    Update App.vue
    bug fix

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -4,7 +4,7 @@
         <div class="container px-6 py-4 mx-auto md:flex md:justify-between md:items-center">
             <div class="flex items-center justify-between">
                 <div>
-                    <a class="text-2xl font-bold text-gray-800 dark:text-white lg:text-3xl hover:text-gray-700 dark:hover:text-gray-300" href="#">Blog</a>
+                    <router-link to="/" class="text-2xl font-bold text-gray-800 dark:text-white lg:text-3xl hover:text-gray-700 dark:hover:text-gray-300" >Blog</router-link>
                 </div>
 
                 <!-- Mobile menu button -->
